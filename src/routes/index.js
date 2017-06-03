ca4f4377e444720685bdaf8d4c7247a9f805358b
cruz30 <cruz30@purdue.edu>
2017-06-03 14:12:10 -0500

    Added skeleton code for api

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import config from '../config';
+import middleware from '../middleware';
+import initializeDb from '../db';
+
+let router = express();
+
+// connect to db
+initializeDb(db => {
+
+  // initialize middleware
+  router.use(middleware({config, db}));
+
+  // api routes v1
+
+});
+
+export default router;
